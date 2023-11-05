94093278f253a647fa0f49ffb0fbb43b3108598f
setamies <lteerimaki@gmail.com>
2023-11-05 17:04:19 +0200

    Small updates in data

---
+++ data/macro_data.py
@@ -0,0 +1,62 @@
+import pandas as pd
+import yfinance as yf
+
+def to_timezone_naive(datetime_series):
+    return datetime_series.dt.tz_localize(None)
+
+def get_month_end_data(df, date_column='Date'):
+    df = df.copy()
+    df[date_column] = to_timezone_naive(pd.to_datetime(df[date_column]))
+    df = df.sort_values(by=date_column)
+    df['YearMonth'] = df[date_column].dt.to_period('M')
+    monthly_df = df.groupby('YearMonth').last()
+    monthly_df.reset_index(inplace=True)
+    monthly_df[date_column] = monthly_df['YearMonth'].dt.strftime('%Y-%m')
+    monthly_df.drop('YearMonth', axis=1, inplace=True)
+    return monthly_df
+
+def download_and_process_financial_data(ticker, start_date, end_date, column_name):
+    data = yf.download(ticker, start=start_date, end=end_date)['Close'].reset_index()
+    data = get_month_end_data(data)
+    data = data.rename(columns={'Close': column_name})
+    return data
+
+def calculate_yield_curve_slope(tbill_3_month, tbill_10_yr):
+    ycurve_slope = tbill_10_yr['10 Year T-Bill Rate'] - tbill_3_month['3 Month T-Bill Rate']
+    return pd.DataFrame({'Date': tbill_3_month['Date'], 'Yield Curve Slope': ycurve_slope})
+
+def process_reer_data(filepath):
+    reer_data = pd.read_excel(filepath, header=10)
+    reer_data = reer_data[['observation_date', 'RBUSBIS']].rename(columns={'observation_date': 'Date'})
+    reer_data['Reer Change'] = reer_data['RBUSBIS'].pct_change()
+    return get_month_end_data(reer_data, 'Date')[['Date', 'Reer Change']]
+
+def process_cpi_data(filepath):
+    cpi_data = pd.read_csv(filepath)
+    cpi_data = cpi_data[cpi_data['LOCATION'] == 'USA']
+    cpi_data = cpi_data.rename(columns={'TIME': 'Date', 'Value': 'CPI'})
+    return get_month_end_data(cpi_data[['Date', 'CPI']], 'Date')
+
+# Download and process financial data
+short_term_interest = download_and_process_financial_data('^IRX', '2005-12-01', '2023-08-01', '3 Month T-Bill Rate')
+long_term_interest = download_and_process_financial_data('^TNX', '2005-12-01', '2023-08-01', '10 Year T-Bill Rate')
+oil_price = download_and_process_financial_data('CL=F', '2005-12-01', '2023-08-01', 'Oil Price')
+oil_price['Change Oil Price'] = oil_price['Oil Price'].pct_change()
+
+# Calculate the yield curve slope
+ycurve_slope = calculate_yield_curve_slope(short_term_interest, long_term_interest)
+
+# Process REER and CPI data
+reer_data = process_reer_data('data/dollar_reer.xls')
+us_cpi_data = process_cpi_data('data/cpi.csv')
+
+# Merge all dataframes on the Date column
+macro_df = (short_term_interest.merge(ycurve_slope, on='Date', how='left')
+                               .merge(reer_data, on='Date', how='left')
+                               .merge(oil_price[['Date', 'Change Oil Price']], on='Date', how='left')
+                               .merge(us_cpi_data, on='Date', how='left')
+                               .dropna()
+                               .rename(columns={'Date': 'Year-Month'}))
+
+# Save the macro data to a csv file
+macro_df.to_csv('data/macro_data.csv', index=False)
--- data/stock_data.py
+++ data/stock_data.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import yfinance as yf
 import numpy as np
 
@@ -19,17 +20,35 @@ class StockData:
 
     def calculate_log_return(self):
         # Could also calculate as the differnece in returns
-        self.data['Log Return'] = np.log(self.data['Adj Close'] / self.data['Adj Close'].shift(1))
+        self.data['Log Return'] = np.log(self.data['Adj Close'] / self.data['Adj Close'].shift(1)) * 100
 
     def calculate_squared_return(self):
         # Proxy for realized variance
         self.data['Squared Return'] = self.data['Log Return']**2
 
+    def format_date(self):
+        # Format the date column to be YYYY-MM-DD
+        self.data.reset_index(inplace=True)
+        self.data['Date'] = self.data['Date'].dt.strftime('%Y-%m-%d')
+
+    def choose_data(self, columns):
+        # Choose only the specified columns
+        self.data = self.data[columns]
+
+    def set_date_as_index(self):
+        # Set the date column as the index
+        self.data.set_index('Date', inplace=True)
+        # Turn the index to datetime
+        self.data.index = pd.to_datetime(self.data.index)
+
     def fetch_data(self):
         """
         Run all calculations on the fetched data.
         """
+        self.format_date()
         self.calculate_daily_return()
         self.calculate_log_return()
         self.calculate_squared_return()
+        self.choose_data(['Date', 'Log Return', 'Squared Return'])
+        self.set_date_as_index()
         return self.data
